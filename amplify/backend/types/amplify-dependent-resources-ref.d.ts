f472876fa4c3372c125a5a4253ed7ffc7bb26ce5
aayutha <thatheraaayushi@gmail.com>
2022-06-17 00:45:18 +0530

    Amplify storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "aayushiworkflow": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "aayushiworkflowe8132718": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "demodocuments": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment,useState,useCallback,useEffect} from "react";
+// import ReactFlow, { addEdge, Background,Controls,MiniMap,useEdgesState,useNodesState, StepEdge,Handle,Position } from "react-flow-renderer";
+import {API,Auth, graphqlOperation,Storage, label} from "aws-amplify";
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import * as queries from './graphql/queries';
+import * as mutations from './graphql/mutations'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+	const[fileName,setfileName]=useState("");
+	const[filePath,setfilePath]=useState("");
+	useEffect(()=>{
+		// fetchName()
+	},[])
+async function fetchName(){
+	const name=await API.graphql(graphqlOperation(queries.listTodos))
+	const files=name.data.listTodos.items
+	// console.log(files)
+	const {fileAccessURL} = await Storage.get("sample.pdf");
+	console.log('access url', fileAccessURL);
 }
+return(
+  <button onClick={()=>fetchName()}>Get file</button>
+)
+}
+
 
 export default App;
