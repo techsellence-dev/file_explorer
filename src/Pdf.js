632de97b825b46f065f352956d6a4406f5201a2a
aayutha <thatheraaayushi@gmail.com>
2022-06-21 15:58:37 +0530

    fileviewer

---
+++ src/Pdf.js
@@ -0,0 +1,32 @@
+// import { Worker } from '@react-pdf-viewer/core'
+// // Import the main component
+// import { Viewer } from '@react-pdf-viewer/core';
+
+// // Import the styles
+// import '@react-pdf-viewer/core/lib/styles/index.css';
+
+// // Your render function
+// <Viewer fileUrl="sample.pdf" />;
+
+
+
+// const Pdf = () => {
+//     return(
+//         <>
+//      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
+
+//      <div
+//     style={{
+//         border: '1px solid rgba(0, 0, 0, 0.3)',
+//         height: '750px',
+//     }}
+// >
+//     <Viewer fileUrl="sample.pdf" />
+// </div>
+//      </Worker>
+    
+//      </>
+//     )
+// };
+
+// export default Pdf
