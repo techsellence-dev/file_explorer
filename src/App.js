a0e7d5ad5ea5ab4dd56b11f50d634e4d0e59865d
aayutha <thatheraaayushi@gmail.com>
2022-06-22 18:31:34 +0530

    icon

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@
 // // import DocViewer from "react-doc-viewer";
 // import * as queries from './graphql/queries';
 // import * as mutations from './graphql/mutations'
-// import Pdf from "./Pdf";
+ import Pdf from "./Pdf";
 // import { configure } from "@testing-library/react";
-// const App=()=>{
+ const App=()=>{
 
 // 	const[fileName,setfileName]=useState("");
 // 	const[filePath,setfilePath]=useState("");
@@ -41,7 +41,7 @@
 // }
 	
 
-// return(
+return(
 // 	<div>
   
 //   <div>
@@ -54,142 +54,148 @@
 //    }
 // </div>
 //   <div>
-// 	{/* <Pdf></Pdf> */}
+    <Pdf></Pdf> 
 // 	</div> 
 //   </div>
 
-// )
-// }
+)
+ }
 
 
-// export default withAuthenticator(App);
+//  export default withAuthenticator(App);
+ export default App;
+// import React from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+// import TreeView from "@material-ui/lab/TreeView";
+// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+// import TreeItem from "@material-ui/lab/TreeItem";
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import FileCopyIcon from '@mui/icons-material/FileCopy';
+// // import TreeView from '@mui/lab/TreeView';
+// import { styled } from '@mui/material/styles';
+// import  { treeItemClasses } from '@mui/lab/TreeItem';
+// const useViewStyles = makeStyles({
+//   root: {}
+// });
 
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TreeView from "@material-ui/lab/TreeView";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import TreeItem from "@material-ui/lab/TreeItem";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-const useViewStyles = makeStyles({
-  root: {}
-});
+// const useItemStyles = makeStyles(theme => ({
+//   root: {
+//     "& > .MuiTreeItem-content > .MuiTreeItem-label": {
+//       display: "flex",
+//       alignItems: "center",
+//       padding: "4px 0",
+//       background: "transparent !important",
+//       pointerEvents: "none"
+//     },
+//     "& > .MuiTreeItem-content  > .MuiTreeItem-label::before": {
+//       content: "''",
+//       display: "inline-block",
+//       width: 12,
+//       height: 12,
+//       marginRight: 8,
+//       border: "1px solid #ccc",
+//       background: "white"
+//     }
+//   },
+//   iconContainer: {
+//     marginRight: 12,
+//     "& > svg": {
+//       padding: 8,
+//       "&:hover": {
+//         opacity: 0.6
+//       }
+//     }
+//   },
+//   label: {
+//     padding: 0
+//   },
+//   selected: {
+//     "& > .MuiTreeItem-content  > .MuiTreeItem-label::before": {
+//       background: theme.palette.primary.main,
+//       border: "1px solid transparent"
+//     }
+//   }
+// }));
 
-const useItemStyles = makeStyles(theme => ({
-  root: {
-    "& > .MuiTreeItem-content > .MuiTreeItem-label": {
-      display: "flex",
-      alignItems: "center",
-      padding: "4px 0",
-      background: "transparent !important",
-      pointerEvents: "none"
-    },
-    "& > .MuiTreeItem-content  > .MuiTreeItem-label::before": {
-      content: "''",
-      display: "inline-block",
-      width: 12,
-      height: 12,
-      marginRight: 8,
-      border: "1px solid #ccc",
-      background: "white"
-    }
-  },
-  iconContainer: {
-    marginRight: 12,
-    "& > svg": {
-      padding: 8,
-      "&:hover": {
-        opacity: 0.6
-      }
-    }
-  },
-  label: {
-    padding: 0
-  },
-  selected: {
-    "& > .MuiTreeItem-content  > .MuiTreeItem-label::before": {
-      background: theme.palette.primary.main,
-      border: "1px solid transparent"
-    }
-  }
-}));
+// export default function ControlledTreeView() {
+//   const classesView = useViewStyles();
+//   // const classesItem = URL("")
 
-export default function ControlledTreeView() {
-  const classesView = useViewStyles();
-  const classesItem = useItemStyles();
+//   const [expanded, setExpanded] = React.useState([]);
+//   const [selected, setSelected] = React.useState([]);
 
-  const [expanded, setExpanded] = React.useState([]);
-  const [selected, setSelected] = React.useState([]);
+//   const handleToggle = (event, nodeIds) => {
+//     if (event.target.nodeName !== "svg") {
+//       return;
+//     }
+//     setExpanded(nodeIds);
+//   };
 
-  const handleToggle = (event, nodeIds) => {
-    if (event.target.nodeName !== "svg") {
-      return;
-    }
-    setExpanded(nodeIds);
-  };
-
-  const handleSelect = (event, nodeIds) => {
-    if (event.target.nodeName === "svg") {
-      return;
-    }
-    const first = nodeIds[0];
-    if (selected.includes(first)) {
-      setSelected(selected.filter(id => id !== first));
-    } else {
-      setSelected([first, ...selected]);
-    }
-  };
-  const handleExpandClick = () => {
-	    setExpanded((oldExpanded) =>
-	      oldExpanded.length === 0 ? ["1", "5", "6", "7", "10", "11"] : []
-	    );
-	  };
+//   const handleSelect = (event, nodeIds) => {
+//     if (event.target.nodeName === "svg") {
+//       return;
+//     }
+//     const first = nodeIds[0];
+//     if (selected.includes(first)) {
+//       setSelected(selected.filter(id => id !== first));
+//     } else {
+//       setSelected([first, ...selected]);
+//     }
+//   };
+//   const handleExpandClick = () => {
+// 	    setExpanded((oldExpanded) =>
+// 	      oldExpanded.length === 0 ? ["1", "5", "6", "7", "10", "11"] : []
+// 	    );
+// 	  };
 	
-	  const handleSelectClick = () => {
-	    setSelected((oldSelected) =>
-	      oldSelected.length === 0
-	        ? ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-	        : []
-	    );
-	  };
+// 	  const handleSelectClick = () => {
+// 	    setSelected((oldSelected) =>
+// 	      oldSelected.length === 0
+// 	        ? ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+// 	        : []
+// 	    );
+// 	  };
 
-  return (
-  <Box sx={{ height: 570, flexGrow: 1, maxWidth: 800, overflowY: "auto" }}>
-       <Box sx={{ mb: 1 }}>
-         <Button onClick={handleExpandClick}>
-           {expanded.length === 0 ? "Expand all" : "Collapse all"}
-         </Button>
-         <Button onClick={handleSelectClick}>
-           {selected.length === 0 ? "Select all" : "Unselect all"}
-         </Button>
-      </Box>
+//   return (
+//   <Box sx={{ height: 570, flexGrow: 1, maxWidth: 800, overflowY: "auto" }}>
+//        <Box sx={{ mb: 1 }}>
+//          <Button onClick={handleExpandClick}>
+//            {expanded.length === 0 ? "Expand all" : "Collapse all"}
+//          </Button>
+//          <Button onClick={handleSelectClick}>
+//            {selected.length === 0 ? "Select all" : "Unselect all"}
+//          </Button>
+//       </Box>
 	  
-    <TreeView
-      classes={classesView}
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      expanded={expanded}
-      selected={selected}
-      onNodeToggle={handleToggle}
-      onNodeSelect={handleSelect}
-      multiSelect
-    >
-      <TreeItem classes={classesItem} nodeId="1" label="Applications">
-        <TreeItem classes={classesItem} nodeId="2" label="Calendar" />
-        <TreeItem classes={classesItem} nodeId="3" label="Chrome" />
-        <TreeItem classes={classesItem} nodeId="4" label="Webstorm" />
-      </TreeItem>
-      <TreeItem classes={classesItem} nodeId="5" label="Documents">
-        <TreeItem classes={classesItem} nodeId="6" label="Material-UI">
-          <TreeItem classes={classesItem} nodeId="7" label="src">
-            <TreeItem classes={classesItem} nodeId="8" label="index.js" />
-            <TreeItem classes={classesItem} nodeId="9" label="tree-view.js" />
-          </TreeItem>
-        </TreeItem>
-      </TreeItem>
-    </TreeView>
-	</Box>
-  );
+//     <TreeView
+//       classes={classesView}
+//       defaultCollapseIcon={<ExpandMoreIcon />}
+//       defaultExpandIcon={<ChevronRightIcon />}
+//       expanded={expanded}
+//       selected={selected}
+//       onNodeToggle={handleToggle}
+//       onNodeSelect={handleSelect}
+//       multiSelect
+//     >
+//       <TreeItem  nodeId="1" label="Applications">
+//         <FileCopyIcon fontSize="small" />
+//         <TreeItem  nodeId="2" label="Calendar"  />
+//         <StyledTreeItem nodeId="1" labelText="All Mail" labelIcon={FileCopyIcon} />
+//         <TreeItem  nodeId="3" label="Chrome" />
+//         <TreeItem  nodeId="4" label="Webstorm" />
+//       </TreeItem>
+//       <TreeItem  nodeId="5" label="Documents">
+//         <TreeItem  nodeId="6" label="Material-UI">
+//           <TreeItem  nodeId="7" label="src">
+//             <TreeItem  nodeId="8" label="index.js" />
+//             <TreeItem  nodeId="9" label="tree-view.js" />
+//           </TreeItem>
+//         </TreeItem>
+//       </TreeItem>
+//     </TreeView>
+// 	</Box>
+//   );
   
-}
+// }
--- src/Pdf.js
+++ src/Pdf.js
@@ -1,32 +1,133 @@
-// import { Worker } from '@react-pdf-viewer/core'
-// // Import the main component
-// import { Viewer } from '@react-pdf-viewer/core';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import TreeView from '@mui/lab/TreeView';
+import TreeItem, { treeItemClasses } from '@mui/lab/TreeItem';
+import Typography from '@mui/material/Typography';
+import MailIcon from '@mui/icons-material/Mail';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  [`& .${treeItemClasses.content}`]: {
+    color: theme.palette.text.secondary,
+    borderTopRightRadius: theme.spacing(2),
+    borderBottomRightRadius: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+    fontWeight: theme.typography.fontWeightMedium,
+    '&.Mui-expanded': {
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    '&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused': {
+      backgroundColor: `var(--tree-view-bg-color, ${theme.palette.action.selected})`,
+      color: 'var(--tree-view-color)',
+    },
+    [`& .${treeItemClasses.label}`]: {
+      fontWeight: 'inherit',
+      color: 'inherit',
+    },
+  },
+  [`& .${treeItemClasses.group}`]: {
+    marginLeft: 0,
+    [`& .${treeItemClasses.content}`]: {
+      paddingLeft: theme.spacing(2),
+    },
+  },
+}));
 
-// // Import the styles
-// import '@react-pdf-viewer/core/lib/styles/index.css';
+function StyledTreeItem(props) {
+  const {
+    bgColor,
+    color,
+    labelIcon: LabelIcon,
+    labelInfo,
+    labelText,
+    ...other
+  } = props;
 
-// // Your render function
-// <Viewer fileUrl="sample.pdf" />;
+  return (
+    <StyledTreeItemRoot
+      label={
+        <Box sx={{ display: 'flex', alignItems: 'center', p: 0.5, pr: 0 }}>
+          <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
+          <Typography variant="body2" sx={{ fontWeight: 'inherit', flexGrow: 1 }}>
+            {labelText}
+          </Typography>
+          <Typography variant="caption" color="inherit">
+            {labelInfo}
+          </Typography>
+        </Box>
+      }
+      style={{
+        '--tree-view-color': color,
+        '--tree-view-bg-color': bgColor,
+      }}
+      {...other}
+    />
+  );
+}
 
+StyledTreeItem.propTypes = {
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
+  labelIcon: PropTypes.elementType.isRequired,
+  labelInfo: PropTypes.string,
+  labelText: PropTypes.string.isRequired,
+};
 
-
-// const Pdf = () => {
-//     return(
-//         <>
-//      <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js">
-
-//      <div
-//     style={{
-//         border: '1px solid rgba(0, 0, 0, 0.3)',
-//         height: '750px',
-//     }}
-// >
-//     <Viewer fileUrl="sample.pdf" />
-// </div>
-//      </Worker>
-    
-//      </>
-//     )
-// };
-
-// export default Pdf
+export default function Pdf() {
+  return (
+    <TreeView
+      aria-label="gmail"
+      defaultExpanded={['3']}
+      defaultCollapseIcon={<ArrowDropDownIcon />}
+      defaultExpandIcon={<ArrowRightIcon />}
+      defaultEndIcon={<div style={{ width: 24 }} />}
+      sx={{ height: 600, flexGrow: 1, maxWidth: 200 }}
+    >
+      <StyledTreeItem nodeId="1" labelText="File1" labelIcon={FileCopyIcon} >file</StyledTreeItem>
+      
+      <StyledTreeItem nodeId="2" labelText="File2" labelIcon={FileCopyIcon} />
+      <StyledTreeItem nodeId="3" labelText="File3" labelIcon={FileCopyIcon}>
+        <StyledTreeItem
+          nodeId="5"
+          labelText="File3-1"
+          labelIcon={FileCopyIcon}
+        //   labelInfo="90"
+          color="#1a73e8"
+          bgColor="#e8f0fe"
+        />
+        <StyledTreeItem
+          nodeId="6"
+          labelText="File3-2"
+          labelIcon={FileCopyIcon}
+        //   labelInfo="2,294"
+          color="#e3742f"
+          bgColor="#fcefe3"
+        />
+        <StyledTreeItem
+          nodeId="7"
+          labelText="File3-3"
+          labelIcon={FileCopyIcon}
+        //   labelInfo="3,566"
+          color="#a250f5"
+          bgColor="#f3e8fd"
+        />
+        <StyledTreeItem
+          nodeId="8"
+          labelText="File3-4"
+          labelIcon={FileCopyIcon}
+        //   labelInfo="733"
+          color="#3c8039"
+          bgColor="#e6f4ea"
+        />
+      </StyledTreeItem>
+      <StyledTreeItem nodeId="4" labelText="File4" labelIcon={FileCopyIcon} />
+    </TreeView>
+  );
+}
